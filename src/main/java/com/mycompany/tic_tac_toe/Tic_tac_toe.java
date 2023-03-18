8d685c06285186690e6ec6f69f562e8079ec59c8
unknown <mohamad.isa.moghrabi@gmail.com>
2023-03-18 14:13:54 +0200

    getting started with the design

---
+++ src/main/java/com/mycompany/tic_tac_toe/Tic_tac_toe.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tic_tac_toe;
+
+/**
+ *
+ * @author GoldenTech
+ */
+public class Tic_tac_toe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
